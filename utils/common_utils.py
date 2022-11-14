c7cb55a9c05ec426945da4743a9ec9f4801b1b9c
iambalakrishnan <imbkrishnaa@gmail.com>
2022-11-14 18:24:56 +0530

    and model created

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -10,6 +10,7 @@ def prepare_data(df, target_col="y"):
     
     return X,y
 
+
 def save_plot(df, model, filename="plot.png", plot_dir="plots"):
     def _create_base_plot(df):
         df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="coolwarm")
